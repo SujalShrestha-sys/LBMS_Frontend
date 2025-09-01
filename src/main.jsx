2602a43821b767c36070fec3a64167ca4f633841
sujal shestha <sujal84071@gmail.com>
2025-09-01 23:59:07 +0545

    changes No-2

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import "./styles.css";
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <>
     <App />
-  </>
-);
+  </>,
+)
